a9d4e1d317f552620d97949bcd7b31b9a4a67908
arminkoeck <armin.koeck@gmail.com>
2024-08-13 11:45:40 +0200

    refactor: move placeShip Code to private function

--- src/gameboard.js
+++ src/gameboard.js
@@ -2,23 +2,29 @@ import { createShip } from "./ships.js";
 
 export function createGameboard() {
   const boardSize = 9;
-  const board = createFields(boardSize);
-  console.log(board);
+  const gameboard = createFields(boardSize);
   let ships = [];
 
-  function getBoardSize () {
+  function getBoard() {
+    return gameboard;
+  }
+
+  function getBoardSize() {
     return boardSize;
   }
 
-  function placeShip(location, length, direction) {
-    let shipFields = [location];
-    if (direction === "x" && ((location[0] + (length - 1)) > boardSize)) {
-      return
-    }
-    if (direction === "y" && (location[1] + (length - 1)) > boardSize) {
-      return
-    }
-    for (let i = 0; i < (length - 1); i++) {
+  // function _checkFieldForShip(newFields, curShips) {
+  //   function arraysHaveEqualSubArray(arr1, arr2) {
+  //     return arr1.some((subArray1) =>
+  //       arr2.some(
+  //         (subArray2) => JSON.stringify(subArray1) === JSON.stringify(subArray2)
+  //       )
+  //     );
+  //   }
+  // }
+
+  function _getShipFields (shipFields,length, direction) {
+    for (let i = 0; i < length - 1; i++) {
       if (direction === "x") {
         let nextField = [shipFields[i][0] + 1, shipFields[i][1]];
         shipFields.push(nextField);
@@ -27,6 +33,20 @@ export function createGameboard() {
         shipFields.push(nextField);
       }
     }
+  }
+
+  function placeShip(location, length, direction) {
+    if (direction === "x" && location[0] + (length - 1) > boardSize) {
+      return;
+    }
+    if (direction === "y" && location[1] + (length - 1) > boardSize) {
+      return;
+    }
+
+    let shipFields = [location];
+    _getShipFields(shipFields, length, direction)
+    
+    // _checkFieldForShip (shipFields, ships)
     ships.push({ ship: createShip(length), coordinates: shipFields });
   }
 
@@ -34,11 +54,24 @@ export function createGameboard() {
     return ships;
   }
 
-  function receiveAttack (field) {
-    ships[0].ship.hit();
+  function receiveAttack(field) {
+    if (gameboard[field[1]][field[0]].status === "hit") {
+      return null;
+    } else {
+      gameboard[field[1]][field[0]].status = "hit";
+      for (let i = 0; i < ships.length; i++) {
+        let ship = ships[i].ship;
+        let coordinates = ships[i].coordinates;
+        for (let j = 0; j < coordinates.length; j++) {
+          if (JSON.stringify(coordinates[j]) === JSON.stringify(field)) {
+            ship.hit();
+          }
+        }
+      }
+    }
   }
 
-  return { placeShip, listShips, receiveAttack, getBoardSize };
+  return { placeShip, listShips, receiveAttack, getBoardSize, getBoard };
 }
 
 function createFields(boardSize) {
@@ -46,12 +79,10 @@ function createFields(boardSize) {
   for (let i = 0; i <= boardSize; i++) {
     let row = [];
     for (let j = 0; j <= boardSize; j++) {
-      let field = { value: null, status: "open" };
+      let field = { status: null };
       row.push(field);
     }
     board.push(row);
   }
   return board;
 }
-
-createGameboard();
